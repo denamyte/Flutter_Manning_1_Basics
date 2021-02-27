e0a49e7ee0e727e7771e7c7328e8bfef5bd41042
denamyte <denamyte@gmail.com>
2021-02-28 01:34:35 +0200

    2021-02-27: Lesson 7. Booring...

--- lib/location_list.dart
+++ lib/location_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './models/location.dart';
+import './styles.dart';
 
 class LocationList extends StatelessWidget {
   final List<Location> locations;
@@ -9,13 +10,35 @@ class LocationList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // title: Text(location.name),
-        // todo Go on here
-        //  https://fluttercrashcourse.com/courses/basics/lessons/lists-listview-listtile
-        //  3:24
-
+      appBar: AppBar(title: Text("Locations", style: Styles.navBarTitle)),
+      body: ListView.builder(
+        itemCount: this.locations.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            contentPadding: EdgeInsets.all(10),
+            leading: _itemThumbnail(this.locations[index]),
+            title: _itemTitle(this.locations[index]),
+          );
+        },
       ),
+
+    );
+  }
+
+  Widget _itemThumbnail(Location location) {
+    return Container(
+      constraints: BoxConstraints.tightFor(width: 100.0),
+      child: Image.network(
+        location.url,
+        fit: BoxFit.fitHeight,
+      ),
+    );
+  }
+
+  Widget _itemTitle(Location location) {
+    return Text(
+      '-= ${location.name} =-',
+      style: Styles.textDefault,
     );
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import './location_list.dart';
 import './models/location.dart';
 import 'location_detail.dart';
 import './mocks/mock_location.dart';
 
 void main() {
-  final Location mockLocation = MockLocation.FetchAny();
+  final mockLocations = MockLocation.fetchAll();
 
-  runApp(MaterialApp(
-    home: LocationDetail(mockLocation)));
+  runApp(MaterialApp(home: LocationList(mockLocations)));
 }
